b323fe60523c0ecb020cf40931199b740edb92cb
clement-jny <82049263+clement-jny@users.noreply.github.com>
2023-04-28 12:44:10 +0200

    Creating express routes

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,9 @@
-const http = require("http"); //import { createServer } from "http"; || import * as http from "http";
-const url = require("url");
+//const http = require("http"); //import { createServer } from "http"; || import * as http from "http";
 const mysql = require('mysql'); //import * as mysql from "mysql";
 require("dotenv").config();// import * as dotenv from "dotenv"; || const dotenv = require("dotenv");
 
 const express = require("express");
-
 const app = express();
-
-app.get("/", (req, res) => {
-	res.setHeader("Content-Type", "application/json");
-	res.status(200).send({ "message": "page /" });
-})
-
-
-app.listen(3000, "localhost", () => {
-	console.log('Serveur en écoute sur le port 3000.');
-})
-//"start": "nodemon --watch app.js app.js"
 
 
 const connection = mysql.createConnection({
@@ -33,55 +20,113 @@ connection.connect((err) => {
 		return;
 	}
 	console.log('Connecté à la base de données.');
-})
+});
 
 
 
-const requestListener = (req, res) => {
-	res.setHeader("Content-Type", "application/json");
+app.get("/", (req, res) => {
+	res.status(200).json({message: "Connected!"});
+});
+app.get("/users", (req , res) => {
+	if (Object.keys(req.query).length !== 0) {
+		const login = req.query.login.toLowerCase() + "%";
 
-	switch (req.url) {
-		case "/users":
-			connection.query("select * from user", (err, rows) => {
-				if (err) {
-					console.error('Erreur de requête SQL : ' + err.stack);
-					res.writeHead(500);
-					res.end(`{"message": "Erreur de serveur."}`);
-					return;
-				}
-
-				//res.end(JSON.parse(JSON.stringify(rows[0])));
-				//console.log(structuredClone(rows[0])); //JSON.parse(JSON.stringify(rows))
-				//res.end(structuredClone(rows[0]));
-				res.end(JSON.stringify(rows));
-			})
-			break;
-
-		case "/hello":
-			//console.log(url.parse(req.url, true).pathname.replace(/^\/+|\/+$/g, ''));
-			const parsedUrl = url.parse(req.url, true);
-			const path = parsedUrl.pathname;
-			const trimmedPath = path.replace(/^\/+|\/+$/g, '');
-			console.log(trimmedPath);
-
-
-			if (trimmedPath === 'hello') {
-				const name = parsedUrl.query.name || 'World';
-				res.setHeader('Content-Type', 'application/json');
-				res.end(JSON.stringify({ message: `Hello, ${name}!` }));
-			} else {
-				res.writeHead(404);
-				res.end();
+		connection.query("select * from user where login like ?", login, (err, rows) => {
+			if (err) {
+				console.error('Erreur de requête SQL : ' + err.stack);
+				res.status(500).json({message: "Erreur de serveur."});
+				return;
 			}
-			break;
-
-
-		default:
-			res.writeHead(200);
-			res.end(`{"message": "Connected!"}`);
-			break;
+	
+			res.status(200).json(rows);
+		});
+	} else {
+		connection.query("select * from user", (err, rows) => {
+			if (err) {
+				console.error('Erreur de requête SQL : ' + err.stack);
+				res.status(500).json({message: "Erreur de serveur."});
+				return;
+			}
+	
+			//res.end(JSON.parse(JSON.stringify(rows[0])));
+			//console.log(structuredClone(rows[0])); //JSON.parse(JSON.stringify(rows))
+			//res.end(structuredClone(rows[0]));
+			//res.end(JSON.stringify(rows));
+	
+			res.status(200).json(rows);
+		});
 	}
-};
+});
+app.get("/users/:id", (req, res) => {
+	const userId = req.params.id;
+
+	connection.query("select * from user where id = ?", userId, (err, rows) => {
+		if (err) {
+			console.error('Erreur de requête SQL : ' + err.stack);
+			res.status(500).json({message: "Erreur de serveur."});
+			return;
+		}
+
+		res.status(200).json(rows);
+	});
+});
+
+
+app.listen(process.env.SERVER_PORT, process.env.SERVER_HOST, () => {
+	console.log(`Server is running on http://${process.env.SERVER_HOST}:${process.env.SERVER_PORT}`);
+})
+//"start": "nodemon --watch app.js app.js"
+
+
+
+
+
+
+// const requestListener = (req, res) => {
+// 	res.setHeader("Content-Type", "application/json");
+
+// 	switch (req.url) {
+// 		case "/users":
+// 			connection.query("select * from user", (err, rows) => {
+// 				if (err) {
+// 					console.error('Erreur de requête SQL : ' + err.stack);
+// 					res.writeHead(500);
+// 					res.end(`{"message": "Erreur de serveur."}`);
+// 					return;
+// 				}
+
+// 				//res.end(JSON.parse(JSON.stringify(rows[0])));
+// 				//console.log(structuredClone(rows[0])); //JSON.parse(JSON.stringify(rows))
+// 				//res.end(structuredClone(rows[0]));
+// 				res.end(JSON.stringify(rows));
+// 			});
+// 			break;
+
+// 		case "/hello":
+// 			//console.log(url.parse(req.url, true).pathname.replace(/^\/+|\/+$/g, ''));
+// 			const parsedUrl = url.parse(req.url, true);
+// 			const path = parsedUrl.pathname;
+// 			const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+// 			console.log(trimmedPath);
+
+
+// 			if (trimmedPath === 'hello') {
+// 				const name = parsedUrl.query.name || 'World';
+// 				res.setHeader('Content-Type', 'application/json');
+// 				res.end(JSON.stringify({ message: `Hello, ${name}!` }));
+// 			} else {
+// 				res.writeHead(404);
+// 				res.end();
+// 			}
+// 			break;
+
+
+// 		default:
+// 			res.writeHead(200);
+// 			res.end(`{"message": "Connected!"}`);
+// 			break;
+// 	}
+// };
 
 // Fermer la connexion à la base de données
 // connection.end((err) => {
@@ -92,6 +137,6 @@ const requestListener = (req, res) => {
 // 	console.log('Déconnecté de la base de données.');
 // });
 
-http.createServer(requestListener).listen(process.env.SERVER_PORT, process.env.SERVER_HOST, () => {
-	console.log(`Server is running on http://${process.env.SERVER_HOST}:${process.env.SERVER_PORT}`);
-});
+// http.createServer(requestListener).listen(process.env.SERVER_PORT, process.env.SERVER_HOST, () => {
+// 	console.log(`Server is running on http://${process.env.SERVER_HOST}:${process.env.SERVER_PORT}`);
+// });
